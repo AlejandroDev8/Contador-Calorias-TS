1fd4d53a5336d8fa274eb3fc9d14204c8e82f2a0
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-01-12 21:13:07 -0600

    Creando el 'state' para las actividades

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,14 +1,24 @@
+import { useState } from "react"
 import { categories } from "../data/categories"
 
 export const Form = () => {
+
+  const [activity, setActivity] = useState({
+    category: 0,
+    name: '',
+    calories: 0
+  })
+
   return (
     <section className="bg-lime-500 py-20 px-5">
       <div className="max-w-4xl mx-auto">
         <form className="space-y-5 bg-white shadow p-10 rounded-lg">
           <div className="grid grid-cols-1 gap-3">
             <label htmlFor="category" className="font-bold">Categorías:</label>
-            <select id="category" className="border border-slate-300 p-2 rounded-lg w-full bg-white">
-              <option disabled selected>Selecciona una categoría</option>
+            <select id="category" className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+              value={activity.category}
+            >
+              <option disabled selected value='0'>Selecciona una categoría</option>
               {categories.map(category => (
                 <option
                   key={category.id}
@@ -20,12 +30,13 @@ export const Form = () => {
             </select>
           </div>
           <div className="grid grid-cols-1 gap-3">
-            <label htmlFor="activity" className="font-bold">Actividad:</label>
+            <label htmlFor="name" className="font-bold">Actividad:</label>
             <input
               type="text"
-              id="activity"
+              id="name"
               className="border border-slate-300 p-2 rounded-lg w-full"
               placeholder="Ej. Comida, bebida, transporte, etc."
+              value={activity.name}
             />
           </div>
           <div className="grid grid-cols-1 gap-3">
@@ -36,6 +47,7 @@ export const Form = () => {
               className="border border-slate-300 p-2 rounded-lg w-full"
               placeholder="Ej. 100"
               min="1"
+              value={activity.calories}
             />
           </div>
           <input
